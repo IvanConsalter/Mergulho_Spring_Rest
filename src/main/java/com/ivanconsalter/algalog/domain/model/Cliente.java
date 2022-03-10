d712730231c779d5052c6fe8ed6540ff82ebf545
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-10 10:07:24 -0300

    added annotations in Cliente Class from javax.persistence

--- src/main/java/com/ivanconsalter/algalog/domain/model/Cliente.java
+++ src/main/java/com/ivanconsalter/algalog/domain/model/Cliente.java
@@ -1,17 +1,32 @@
 package com.ivanconsalter.algalog.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @AllArgsConstructor
+@Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String nome;
 	private String email;
 	private String telefone;
+	
+	public Cliente() {
+	}
 
 }
