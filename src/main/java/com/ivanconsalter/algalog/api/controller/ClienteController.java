9bc8eec2b456ea1a9bb291363fdff24ed794b8e5
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-10 10:11:38 -0300

    added EntityManager && createQuery to return List result in ClienteController

--- src/main/java/com/ivanconsalter/algalog/api/controller/ClienteController.java
+++ src/main/java/com/ivanconsalter/algalog/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.ivanconsalter.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,15 @@ import com.ivanconsalter.algalog.domain.model.Cliente;
 
 @RestController
 public class ClienteController {
-	
+
+	@PersistenceContext
+	private EntityManager manager;
+
 	@GetMapping(path = "/clientes")
 	public List<Cliente> listarTodos() {
-		
-		Cliente cliente1 = new Cliente(1L, "João", "99 99999-9999", "joao@gmail.com");
-		Cliente cliente2 = new Cliente(2L, "Maria", "88 88888-8888", "maria@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 
 }
