125b5eb1068e32b2d9969bf0c7adf9b22bdde508
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-11 07:26:18 -0300

    added ClienteRepository

---
+++ src/main/java/com/ivanconsalter/algalog/domain/repository/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.ivanconsalter.algalog.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ivanconsalter.algalog.domain.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+}
