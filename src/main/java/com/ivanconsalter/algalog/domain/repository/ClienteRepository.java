558d6bb7d17eb08847874ff965c8301ab52425e2
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-11 07:36:54 -0300

    added findByNomeContaining method in CLienteRepository

--- src/main/java/com/ivanconsalter/algalog/domain/repository/ClienteRepository.java
+++ src/main/java/com/ivanconsalter/algalog/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.ivanconsalter.algalog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ivanconsalter.algalog.domain.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+	
+	List<Cliente> findByNomeContaining(String nome);
 	
 }
