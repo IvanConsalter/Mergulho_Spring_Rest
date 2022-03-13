6fdf65ff6f3d1f3f3f3cd970c3af26a60bb6e41c
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-13 12:00:42 -0300

    added method to delete a cliente in ClienteController

--- src/main/java/com/ivanconsalter/algalog/api/controller/ClienteController.java
+++ src/main/java/com/ivanconsalter/algalog/api/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -74,6 +75,18 @@ public class ClienteController {
 		cliente = clienteRepository.save(cliente);
 		
 		return ResponseEntity.ok(cliente);
+	}
+	
+	@DeleteMapping(path = "/{id}")
+	public ResponseEntity<Void> remover(@PathVariable Long id) {
+		
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		clienteRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 
 }
