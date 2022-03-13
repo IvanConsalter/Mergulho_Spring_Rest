1c94c514467833b323a66d48706ea9fae9a7f027
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-13 12:21:02 -0300

    added @Valid annotation in ClienteController

--- src/main/java/com/ivanconsalter/algalog/api/controller/ClienteController.java
+++ src/main/java/com/ivanconsalter/algalog/api/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.ivanconsalter.algalog.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -58,14 +60,14 @@ public class ClienteController {
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public Cliente adicionar(@RequestBody Cliente cliente) {
+	public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
 		return clienteRepository.save(cliente);
 	}
 	
 	@PutMapping(path = "/{id}")
 	public ResponseEntity<Cliente> atualizar(
 			@PathVariable Long id,
-			@RequestBody Cliente cliente) {
+			@Valid @RequestBody Cliente cliente) {
 		
 		if(!clienteRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
