d1dbc339fc62859fe2f543c9f7a1c8bef9e0ce11
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-13 15:15:29 -0300

    added class Entrega

---
+++ src/main/java/com/ivanconsalter/algalog/domain/model/Entrega.java
@@ -0,0 +1,44 @@
+package com.ivanconsalter.algalog.domain.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Entrega {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	@Embedded
+	private Destinatario destinatario;
+	
+	private BigDecimal taxa;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusEntrega status;
+	
+	private LocalDateTime dataPedido;
+	private LocalDateTime dataFinalizacao;
+	
+}
