93eee789f00c87784993d26bfedea480a2d8d54c
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-13 12:20:19 -0300

    added validations annotations in Client Class

--- src/main/java/com/ivanconsalter/algalog/domain/model/Cliente.java
+++ src/main/java/com/ivanconsalter/algalog/domain/model/Cliente.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -22,8 +25,17 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(min = 2, max = 60)
 	private String nome;
+	
+	@NotBlank
+	@Email
+	@Size(max = 255)
 	private String email;
+	
+	@NotBlank
+	@Size(max = 20)
 	private String telefone;
 	
 	public Cliente() {
