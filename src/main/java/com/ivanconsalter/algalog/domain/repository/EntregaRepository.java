0da296c867ad250c194a83ea0e91aa1e02c6d360
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-14 10:32:23 -0300

    added EntregaRepository

---
+++ src/main/java/com/ivanconsalter/algalog/domain/repository/EntregaRepository.java
@@ -0,0 +1,11 @@
+package com.ivanconsalter.algalog.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ivanconsalter.algalog.domain.model.Entrega;
+
+@Repository
+public interface EntregaRepository extends JpaRepository<Entrega, Long>{
+
+}
