45562bee88f1bd63d8e50adb49d612e5aa67f1b4
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-15 08:07:14 -0300

    added method listar() && obterPorId() in class EntregaController

--- src/main/java/com/ivanconsalter/algalog/api/controller/EntregaController.java
+++ src/main/java/com/ivanconsalter/algalog/api/controller/EntregaController.java
@@ -1,6 +1,13 @@
 package com.ivanconsalter.algalog.api.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ivanconsalter.algalog.domain.model.Entrega;
+import com.ivanconsalter.algalog.domain.repository.EntregaRepository;
 import com.ivanconsalter.algalog.domain.service.SolicitacaoEntregaService;
 
 import lombok.AllArgsConstructor;
@@ -17,13 +25,26 @@ import lombok.AllArgsConstructor;
 @RequestMapping(path = "/entregas")
 public class EntregaController {
 	
+	private EntregaRepository entregaRepository;
 	private SolicitacaoEntregaService solicitacaoEntregaService;
 	
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
-	public Entrega solicitar(@RequestBody Entrega entrega) {
+	public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
 		
 		return solicitacaoEntregaService.solicitar(entrega);
+	}
+	
+	@GetMapping
+	public List<Entrega> listar() {
+		return entregaRepository.findAll();
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<Entrega> obterPorId(@PathVariable Long id) {
+		return entregaRepository.findById(id)
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
