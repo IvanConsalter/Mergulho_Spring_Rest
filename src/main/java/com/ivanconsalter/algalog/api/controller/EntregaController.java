d2cbb112c6b25fc5cbd7cbed6da1cf6a10bb8355
Ivan Consalter <ivanconsalter1@gmail.com>
2022-03-15 07:17:16 -0300

    added class EntregaController with some annotations

---
+++ src/main/java/com/ivanconsalter/algalog/api/controller/EntregaController.java
@@ -0,0 +1,29 @@
+package com.ivanconsalter.algalog.api.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ivanconsalter.algalog.domain.model.Entrega;
+import com.ivanconsalter.algalog.domain.service.SolicitacaoEntregaService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/entregas")
+public class EntregaController {
+	
+	private SolicitacaoEntregaService solicitacaoEntregaService;
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public Entrega solicitar(@RequestBody Entrega entrega) {
+		
+		return solicitacaoEntregaService.solicitar(entrega);
+	}
+
+}
